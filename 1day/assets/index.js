4aef45f9422cecdc237f46fafe514db31f6726e7
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-10-20 09:21:22 +0300

    refactoring code that working with audio

--- 1day/assets/index.js
+++ 1day/assets/index.js
@@ -1,24 +1,23 @@
 const blocks = document.querySelectorAll(".item");
-const audios = document.querySelectorAll("audio");
 const keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
-blocks.forEach((block, index) => {
-    block.onclick = () => changeClicked(block, index);
+blocks.forEach((block) => {
+    block.onclick = () => changeClicked(block);
 });
 
 document.onkeydown = (event) => {
     for (let i = 0; i < blocks.length; i++) {
-        if (event.key === keys[i]) changeClicked(blocks[i], i);
+        if (event.key === keys[i]) changeClicked(blocks[i]);
     }
 };
 
-function changeClicked(item, index) {
+function changeClicked(item) {
     isClicked = item.classList.contains("item__clicked");
 
     if (isClicked) {
         return;
     } else {
         item.classList.add("item__clicked");
-        audios[index].play();
+        item.querySelector("audio").play();
         setTimeout(() => {
             item.classList.remove("item__clicked");
         }, 123);
